5c875509f078bd116505ea3caa9ff25de9253e11
Krishna Sudhir <69811669+krishnasudhir01@users.noreply.github.com>
2022-04-09 15:37:18 -0400

    parsing all tweets and display

--- twitter.js
+++ twitter.js
@@ -21,8 +21,16 @@ var client = new Twitter({
 var params = {q: 'covid'};
 // var params = {has: 'covid',lang:en,place:state};
 client.get('search/tweets', params, function(error, tweets, response) {
-  console.log(tweets);
+//  console.log(tweets);
+  //const myObj = JSON.parse(tweets.toString());
+
+  for (let i = 0; i < tweets.statuses.length;i++){
+    const stat = tweets.statuses[i];
+    console.log(stat.text);
+    console.log(stat.lang);
+  }
 });
+
 
 //and for queries
 // only log text
