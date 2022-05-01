4eede3b34bfe369cfd222cc269dd1cc3f4d6d19f
Neil Thupili <69744850+Neil589@users.noreply.github.com>
2022-05-01 19:23:24 -0400

    comments added

--- src/twitter.cjs
+++ src/twitter.cjs
@@ -2,6 +2,8 @@ const { text } = require('stream/consumers');
 var Twitter = require('twitter');
 var Sentiment = require('sentiment');
 
+
+//api keys, secret keys and bearer tokens
 const apikey = "7bBZ96F5uNzss9lKSFtMoFiEw"
 const ask = "nuHAMKVJUgLnMEAuAhrP8Makd1UkxJcHU04VUdY1Wbmp5BEfK1"
 const at = "1511452233811996672-jdnn7j7lUB4uagfZwrfYt6eq75tQgU"
@@ -15,15 +17,18 @@ module.exports = function getSentiment(req, res, next) {
     access_token_secret: ats
   });
 
+//creating a search query string to pass into the api
   var str = "covid " + req.params.state + " lang:en"
   var params = { q: str };
+  //using the query string as an api parameter to get seached tweets
   client.get('search/tweets', params, function (error, tweets, response) {
     let allTweets = "";
-
+//taking relevant text out of the tweet information
     for (let i = 0; i < tweets.statuses.length; i++) {
       const stat = tweets.statuses[i];
       allTweets += " " + stat.text;
     }
+    // passing text through the Sentiment analysis API
     var sentimentAPI = new Sentiment();
     var result = sentimentAPI.analyze(allTweets);
     console.dir(result);
